5427c6cb21c4656277c266f44d13411e841a41b4
Hiya Bhavsar <hiyabhavsar@gmail.com>
2023-11-11 14:59:04 -0500

    testing git connection

--- sws.c
+++ sws.c
@@ -2,54 +2,61 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void short_usage() {
+void short_usage()
+{
     fprintf(stderr, "Usage: sws [-dh] [-i address] [-l file] [-p port] <dir>\n");
 }
 
-void usage() {
-
+void usage()
+{
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         short_usage();
         exit(EXIT_FAILURE);
     }
-    
+
     int opt;
     int help = 0;
     int debug = 0;
+    int test = 0;
 
-    while ((opt = getopt(argc, argv, "dhc:i:l:p:")) != -1) {
-        switch(opt) {
-            case 'd':
-                debug = 1;
-                break;
-            case 'h':
-                help = 1;
-                break;
-            case 'c':
-                printf("Execute the CGI here: %s\n", optarg);
-                break;
-            case 'i':
-                printf("Get the IPv4/IPv6 address: %s\n", optarg);
-                break;
-            case 'l':
-                printf("Log the requests into here: %s\n", optarg);
-                break;
-            case 'p':
-                printf("Here's the port number: %s\n", optarg);
-                break;
-            case '?':
-                fprintf(stderr, "sws: unknown option: %c\n", optopt);
-                short_usage();
-                exit(EXIT_FAILURE);
+    while ((opt = getopt(argc, argv, "dhc:i:l:p:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'd':
+            debug = 1;
+            break;
+        case 'h':
+            help = 1;
+            break;
+        case 'c':
+            printf("Execute the CGI here: %s\n", optarg);
+            break;
+        case 'i':
+            printf("Get the IPv4/IPv6 address: %s\n", optarg);
+            break;
+        case 'l':
+            printf("Log the requests into here: %s\n", optarg);
+            break;
+        case 'p':
+            printf("Here's the port number: %s\n", optarg);
+            break;
+        case '?':
+            fprintf(stderr, "sws: unknown option: %c\n", optopt);
+            short_usage();
+            exit(EXIT_FAILURE);
         }
     }
     argc -= optind;
     argv += optind;
 
-    if (help) {
+    if (help)
+    {
         usage();
         exit(EXIT_SUCCESS);
     }
