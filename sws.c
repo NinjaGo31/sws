4feba32e343e177f381e74889427d3d2f27e8dbd
NinjaGo31 <56609280+NinjaGo31@users.noreply.github.com>
2023-11-13 18:07:47 -0500

    Update sws.c

--- sws.c
+++ sws.c
@@ -1,3 +1,7 @@
+#include <arpa/inet.h>
+
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,132 +10,94 @@
 #include <err.h>
 #include <errno.h>
 
-void check_dir(char *path)
-{
-    struct stat *sb;
-    if (stat(path, sb) == -1)
-    {
-        (void)fprintf(stderr, "stat: path does not exist: %s\n", sterror(errno));
-        exit(EXIT_FAILURE);
-    }
-    if (S_ISDIR(sb->st_mode) == 0)
-    {
-        (void)fprintf(stderr, "stat: given path is not directory: %s\n", sterror(errno));
-        exit(EXIT_FAILURE);
-    }
-}
-void short_usage()
-{
-    fprintf(stderr, "Usage: sws [-dh] [-i address] [-l file] [-p port] <dir>\n");
+#include "util.h"
+
+#define ERROR -1
+#define PORT_DEFAULT 8080
+
+static int ip_flags = AF_INET | AF_INET6;
+
+void short_usage() {
+    fprintf(stderr, "Usage: sws [-dh] [-c dir] [-i address] [-l file] [-p port] <dir>\n");
 }
 
-void usage()
-{
-    (void)printf("Usage: sws [-dh] [-i address] [-l file] [-p port] <dir>\n");
+void usage() {
+    (void)printf("Usage: sws [-dh] [-c dir] [-i address] [-l file]");
+    (void)printf(" [-p port] <dir>\n");
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        short_usage();
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        usage();
         exit(EXIT_FAILURE);
     }
-
-    int opt;
-    int help = 0;
-    int debug = 0;
-    int cgi = 0;
-    int ipv = 0;
-    int log = 0;
-    int port = 0;
-
+    
     char *cgi_dir = NULL;
-    char *ipv_address = NULL;
+    char *ip_addr = NULL;
     char *log_file = NULL;
-    int port_num = 8080;
-
+    int cgi = 0, opt = 0, ip = 0, port = 0, help = 0, log = 0, debug = 0;
     int log_fd;
 
-    while ((opt = getopt(argc, argv, "c:dhi:l:p:")) != -1)
-    {
-        switch (opt)
-        {
-        case 'd':
-            debug = 1;
-            break;
-        case 'h':
-            help = 1;
-            break;
-        case 'c':
-            cgi = 1;
-            cgi_dir = optarg;
-            printf("Execute the CGI here: %s\n", cgi_dir);
-            break;
-        case 'i':
-            ipv = 1;
-            ipv_address = optarg;
-            printf("Get the IPv4/IPv6 address: %s\n", ipv_address);
-            break;
-        case 'l':
-            log = 1;
-            log_file = optarg;
-            printf("Log the requests into here: %s\n", log_file);
-            break;
-        case 'p':
-            port = 1;
-            port_num = atoi(optarg);
-            printf("Here's the port number: %s\n", port_num);
-            break;
-        case '?':
-            fprintf(stderr, "sws: unknown option: %c\n", optopt);
-            short_usage();
-            exit(EXIT_FAILURE);
+    while ((opt = getopt(argc, argv, "dhc:i:l:p:")) != -1) {
+        switch(opt) {
+            case 'd':
+                debug = 1;
+                break;
+            case 'h':
+                help = 1;
+                break;
+            case 'c':
+                cgi = 1;
+                cgi_dir = optarg;
+                printf("Execute the CGI here: %s\n", cgi_dir);
+                break;
+            case 'i':
+                ip = 1;
+                ip_addr = optarg;
+                printf("Get the IPv4/IPv6 address: %s\n", ip_addr);
+                break;
+            case 'l':
+                log = 1;
+                log_file = optarg;
+                if (log_fd =
+                    open(log_file, O_CREAT | O_WRONLY | O_APPEND,
+                            S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) < 0) {
+                    fprintf(stderr, "sws: file could not be opened: %s\n",
+                            strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+                printf("Log the requests into here: %s\n", log_file);
+                break;
+            case 'p':
+                if ((parse_port(optarg, &port) == ERROR)) {
+                    exit(EXIT_FAILURE);
+                }
+                if (port < 1024 || port > 65535) {
+                    fprintf(stderr, "sws: Port number must be in range [1024, 65535]\n");
+                    exit(EXIT_FAILURE);
+                }
+                printf("Here's the port number: %s\n", optarg);
+                break;
+            case '?':
+                fprintf(stderr, "sws: unknown option: %c\n", optopt);
+                short_usage();
+                exit(EXIT_FAILURE);
         }
     }
     argc -= optind;
     argv += optind;
 
-    if (help)
-    {
+    if (help) {
         usage();
         exit(EXIT_SUCCESS);
     }
 
-    if (cgi)
-    {
-        if (cgi_dir != NULL)
-        {
-            check_dir(cgi_dir);
-        }
-        else
-        {
+    if (cgi) {
+        if (cgi_dir != NULL) check_dir(cgi_dir);
+        else {
             fprintf(stderr, "directory path not inputted\n");
             exit(EXIT_FAILURE);
         }
-    }
-    if (port)
-    {
-        if (port_num < 1024 || port_num > 65535)
-        {
-            fprintf(stderr, "port number is invalid\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    if (log)
-    {
-        if (log_file != NULL)
-        {
-            if (log_fd = open(log_file, O_CREAT | O_WRONLY | O_APPEND, ) < 0)
-            {
-                fprintf(stderr, "file could not be opened: %s\n", sterror(errno));
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-
-    if (ipv)
-    {
     }
 
     exit(EXIT_SUCCESS);
